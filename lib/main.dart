31297fb9e6887cc9517e071daa982c93faf81bb0
FakhrElddin <fakhereldeanaymanq@gmail.com>
2026-06-25 11:14:03 +0300

    refactor routes

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,8 @@ import 'package:wasel_app/config/app_theme.dart';
 import 'package:wasel_app/core/cache/shared_prefs_utils.dart';
 import 'package:wasel_app/core/di/di.dart';
 import 'package:wasel_app/core/utils/my_bloc_observer.dart';
-import 'package:wasel_app/features/auth/login/presentation/screens/login_screen.dart';
-import 'package:wasel_app/features/auth/register/presentation/screens/register_screen.dart';
-import 'package:wasel_app/features/auth/reset_password/presentation/screens/reset_password_screen.dart';
-import 'package:wasel_app/features/auth/verify_code/presentation/screens/verify_code_screen.dart';
-import 'package:wasel_app/features/onboarding/presentation/screens/onboarding_screen.dart';
 
-import 'features/auth/forget_password/presentation/screens/forget_password_screen.dart';
-import 'features/splash/presentation/screens/splash_screen.dart';
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPrefsUtils.init();
   configureDependencies();
@@ -30,15 +22,7 @@ class WaselApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      routes: {
-        AppRoutes.splashScreenRoute: (context) => SplashScreen(),
-        AppRoutes.onboardingScreenRoute: (context) => OnboardingScreen(),
-        AppRoutes.loginScreenRoute: (context) => LoginScreen(),
-        AppRoutes.registerScreenRoute: (context) => RegisterScreen(),
-        AppRoutes.forgetPasswordScreenRoute: (context) => ForgetPasswordScreen(),
-        AppRoutes.verifyCodeScreenRoute: (context) => VerifyCodeScreen(),
-        AppRoutes.resetPasswordScreenRoute: (context) => ResetPasswordScreen(),
-      },
+      routes: AppRoutes.routes,
       initialRoute: AppRoutes.splashScreenRoute,
     );
   }
